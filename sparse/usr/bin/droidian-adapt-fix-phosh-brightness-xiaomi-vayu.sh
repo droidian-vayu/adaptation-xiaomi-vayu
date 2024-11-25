9f0dcdf9f6779d736a3ba1f63cb829251f0dfa01
berbascum <berbascum@ticv.cat>
2024-11-25 07:01:26 +0100

    (service) fix-phosh-bright: update the script to work in snap 100

--- sparse/usr/bin/droidian-adapt-fix-phosh-brightness-xiaomi-vayu.sh
+++ sparse/usr/bin/droidian-adapt-fix-phosh-brightness-xiaomi-vayu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# TOOT_VERSION 1.0.0.1
+# TOOT_VERSION 1.0.2
 ## Script service to set the screen brightnes to 50% after phosh is started
 #
 # Copyright (C) 2024 Berbascum <berbascum@ticv.cat>
@@ -13,16 +13,27 @@
 ADAPTATION_LOG="/var/log/droidian-adaptation/droidian-adaptation-xiaomi-vayu.log"
 echo "$(date +'%Y-%m-%d %H:%M:%S') Fixing brightness..." | tee -a "${ADAPTATION_LOG}"
 
-brigth_max=$(cat /sys/class/backlight/backlight/max_brightness)
-[ -z "${brigth_max}" ] && exit 1
+## Wait for keyboard to ensure that brightness set takes efect in the phosh session
+kbd_loaded="$(pgrep -ac "wvkbd|osk")"
+while [ "${kbd_loaded}" -eq "0" ]; do
 
-bright_to_set=$((brigth_max / 2))
+    echo "Waiting for keyboard" | tee -a "${ADAPTATION_LOG}"
+    sleep 3
+    kbd_loaded="$(pgrep -ac "wvkbd|osk")"
+done
 
-echo "brigth_max = ${brigth_max}" | tee -a "${ADAPTATION_LOG}"
+
+bright_max="4095"
+bright_lvl="$1"
+[ -z "${bright_lvl}" ] && bright_lvl="60"
+[ "${bright_lvl}" -gt "0" ] || exit 1
+
+bright_to_set=$((bright_max * bright_lvl / 100))
+
+echo "bright_max = ${bright_max}" | tee -a "${ADAPTATION_LOG}"
 echo "bright_to_set = ${bright_to_set}" | tee -a "${ADAPTATION_LOG}"
+echo "bright_lvl = ${bright_lvl}" | tee -a "${ADAPTATION_LOG}"
 
-#echo "2000" | tee /sys/class/backlight/backlight/brightness
-#echo "2000" | tee /sys/class/backlight/panel0-backlight/brightness
 echo "${bright_to_set}" | tee /sys/class/backlight/backlight/brightness
 echo "${bright_to_set}" | tee /sys/class/backlight/panel0-backlight/brightness
 
